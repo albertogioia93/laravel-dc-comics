1b609581088f5f75ec2ec5d75e2f8c60044c7639
albertogioia93 <albertogioia@hotmail.it>
2023-05-10 18:39:02 +0200

    php artisan db:seed ma non riesco a popolare il db

--- database/seeders/ComicsTableSeeder.php
+++ database/seeders/ComicsTableSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Comic;
 
 class ComicsTableSeeder extends Seeder
 {
@@ -14,6 +15,24 @@ class ComicsTableSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $comics = config("comic");
+
+        if (!is_null($comics)) {
+            foreach($comics as $comic) {
+                $newComic = new Comic();
+    
+                $newComic->title = $comic['title'];
+                $newComic->description = $comic['description'];
+                $newComic->thumb = $comic['thumb'];
+                $newComic->price = $comic['price'];
+                $newComic->series = $comic['series'];
+                $newComic->sale_date = $comic['sale_date'];
+                $newComic->type = $comic['type'];
+                $newComic->artists = $comic['artists'];
+                $newComic->writers = $comic['writers'];
+            }
+
+        }
+
     }
 }
