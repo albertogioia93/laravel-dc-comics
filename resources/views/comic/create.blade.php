c29edf7279f71fe332962d97e24811f9577afb31
albertogioia93 <albertogioia@hotmail.it>
2023-05-12 18:48:00 +0200

    feat:aggiunta validazione

--- resources/views/comic/create.blade.php
+++ resources/views/comic/create.blade.php
@@ -13,16 +13,28 @@
         <a href="{{ route('comics.index') }}" class="btn btn-primary btn-sm">Torna all'elenco</a>
     </div>
 
+    <div>
+      @if ($errors->any())
+      <div class="alert alert-danger">
+          <ul>
+              @foreach ($errors->all() as $error)
+                  <li>{{ $error }}</li>
+              @endforeach
+          </ul>
+      </div>
+      @endif
+    </div>
+
     <form action="{{ route('comics.store') }}" method="POST">
         @csrf
         <div class="mb-3">
           <label for="src" class="form-label">Immagine</label>
-          <input type="text" class="form-control" id="src" name="src">
+          <input type="text" class="form-control" id="src" name="src" value="{{ old('src') }}">
         </div>
 
         <div class="mb-3">
             <label for="titolo" class="form-label">Titolo</label>
-            <input type="text" class="form-control" id="titolo" name="titolo">
+            <input type="text" class="form-control" id="titolo" name="titolo" value="{{ old('titolo') }}">
         </div>
 
         <button type="submit" class="btn btn-primary">Submit</button>
--- resources/views/comic/edit.blade.php
+++ resources/views/comic/edit.blade.php
@@ -13,21 +13,33 @@
         <a href="{{ route('comics.index') }}" class="btn btn-primary btn-sm">Torna all'elenco</a>
     </div>
 
+    <div>
+      @if ($errors->any())
+      <div class="alert alert-danger">
+          <ul>
+              @foreach ($errors->all() as $error)
+                  <li>{{ $error }}</li>
+              @endforeach
+          </ul>
+      </div>
+      @endif
+    </div>
+
     <form action="{{ route('comics.update', $comic->id) }}" method="POST">
         @csrf
         @method('PUT')
         <div class="mb-3">
           <label for="src" class="form-label">Immagine</label>
-          <input type="text" class="form-control" id="src" name="src" value="{{ $comic->src }}">
+          <input type="text" class="form-control" id="src" name="src" value="{{ old('src', $comic->src) }}">
         </div>
         <div class="mb-3">
             <label for="id" class="form-label">ID</label>
-            <input type="text" class="form-control" id="id" name="id" value="{{ $comic->id }}">
+            <input type="text" class="form-control" id="id" name="id" value="{{ old('id', $comic->id) }}">
         </div>
 
         <div class="mb-3">
             <label for="titolo" class="form-label">Titolo</label>
-            <input type="text" class="form-control" id="titolo" name="titolo" value="{{ $comic->title }}">
+            <input type="text" class="form-control" id="titolo" name="titolo" value="{{ old('titolo', $comic->titolo) }}">
         </div>
 
         
